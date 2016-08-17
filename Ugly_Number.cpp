fe246f1d5cfa7bdabf4430874e2ba7cccc520131
dbCheng <lilgcheng@gmail.com>
2016-08-17 16:21:33 +0800

    Add files via upload

--- Ugly_Number.cpp
+++ Ugly_Number.cpp
@@ -5,41 +5,30 @@ int main()
 {
 	int n;
 	scanf("%d",&n);
-	isUgly(n);
+	if(isUgly(n)){
+		printf("YES\n");
+	}else{
+		printf("NO\n");
+	}
 	system("pause");
 	return 0;
 }
 
-bool isUgly(int num) {
-	if(num>0 && num<2147483647){
-		int facter=2;
-		int c=0,i=0;
-		int *nums;
-		while(facter<=num){
-			if(num%facter==0){
-				nums = (int*)malloc(sizeof(int)*num);
-				printf("%3d\n",facter);
-				nums[c]= facter;
-				c++;
-			}
-			facter++;
-		}
-		int k=c;
-		printf("\n");
-		printf("c= %d\n",c);
-		for(i=0;i<c;i++){
-			if(nums[i]==2 || nums[i]==3 || nums[i]==5 ){
-				k--;
-			}
-		}
-		if(k==0){
-			printf("YES\n");
-			return true;
-		}else{
-			printf("NO\n");
-			return false;
-		}
-	}else{
+bool isUgly(int n) {
+	if(n == 0){
 		return false;
-	}
+	} 
+	while(n%2 == 0){
+		n /= 2;
+		printf("2/ = %d\n",n);
+	} 
+	while(n%3 == 0){
+		n /= 3;
+		printf("3/ = %d\n",n);
+	} 
+	while(n%5 == 0){
+		n /= 5;
+		printf("5/ = %d\n",n);
+	} 
+	return (n==1)?true:false;
 }
